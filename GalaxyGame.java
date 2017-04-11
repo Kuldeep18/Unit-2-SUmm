73348b42451827f1b81915f52c849d0080d9a154
ElectricJV <DiscipleJV@gmail.com>
2017-04-11 14:32:19 -0400

    Add files via upload

--- GalaxyGame.java
+++ GalaxyGame.java
@@ -34,7 +34,9 @@ public class GalaxyGame {
 		howManyWalls = d4.getValue() + d5.getValue();
 		w = new Wall[howManyWalls];
 
-		//
+		/* For loop which takes values of the rolled dice and places walls based on their values. 
+		 * It will also place the wall in a certain orientation depending on the value of the special 4 faced die.
+		 */
 		for (int i = 0; i < howManyWalls; i++) {
 			d1.rollDie();
 			d2.rollDie();
@@ -73,8 +75,8 @@ public class GalaxyGame {
 		t = new Thing[512];
 
 		/* For loop which rolls the dice and assigns the the added value to thingAve then repeats for thingStr.
-		Then places the thing in the assigned streets and avenues. Repeats until 512 things have been placed down.
-		*/
+		 * Then places the thing in the assigned streets and avenues. Repeats until 512 things have been placed down.
+		 */
 		for (int i = 0; i < 512; i++) {
 			d1.rollDie();
 			d2.rollDie();
@@ -93,14 +95,14 @@ public class GalaxyGame {
 	 */
 	public static void main(String[] args) {
 
-		// Creates a new city called galaxy
+		// Creates a new city called galaxy.
 		City galaxy = new City(0, 0, 15, 15, 40);
 		
-		// Places things and walls within the galaxy
+		// Places things and walls within the galaxy.
 		placeThing(galaxy);	
 		placeWall(galaxy);
 
-		// Creates the droid r2d2 and c3p0
+		// Creates the droid r2d2 and c3p0.
 		Droid r2d2 = new Droid (galaxy, 0, 0, Direction.EAST, 0);
 		Droid c3p0 = new Droid (galaxy, 0, 14, Direction.WEST, 0);
 
@@ -108,12 +110,24 @@ public class GalaxyGame {
 		Die d1 = new Die();
 		Die d2 = new Die();
 		
+		// Declare int variable.
+		int a;
 		
+		// Roll dice to determine the number of things to be picked up
 		d1.rollDie();
 		d2.rollDie();
 		
+		// Assign a the value of the rolled dice to determine how much things the droid must pick up.
+		a = d1.getValue() + d2.getValue();
 		
+		//r2d2.
 
+		d1.rollDie();
+		d2.rollDie();
+		
+		a = d1.getValue() + d2.getValue();
+		
+		//c3p0.
 	}
 
 }
